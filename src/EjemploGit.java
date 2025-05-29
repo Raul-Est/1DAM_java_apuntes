78db1039c8525628ff7122e1bb095dc3f43816b6
Raúl <posadilla@gmail.com>
2025-05-29 10:30:56 +0200

    variables

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -2,8 +2,13 @@
 public class EjemploGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-
+		
+		System.out.println("Hola mundo GitHub");
+		String nombre = "raul";
+		
+		System.out.println(nombre);
+		
+	
 	}
 
 }
